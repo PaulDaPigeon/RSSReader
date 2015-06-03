9aa9e44ac7ec939ab1911df2cbb0ebaae323daa7
Hebok Pal <hebokpal1991@gmail.com>
2015-06-03 18:41:28 +0200

    Added functionality to view articles, though, xml of feed is not yet parsed, but view transitions are working. Right navigation bar item now changes based on view. Added test function to set up feeds with sample data.

--- RSSReader/MasterViewController.m
+++ RSSReader/MasterViewController.m
@@ -11,13 +11,26 @@
 #import "MagicalRecord.h"
 #import "Feed.h"
 
-@interface MasterViewController () <UIAlertViewDelegate>
-@property (strong, nonatomic) NSMutableArray *feeds;
+typedef enum ViewTypes
+{
+    allFeeds = 0,
+    allArticles,
+    articlesOfAFeed
+}ViewType;
 
+@interface MasterViewController () <UIAlertViewDelegate>
+{
+    ViewType viewType;
+    NSMutableArray *feeds;
+    NSMutableArray *articles;
+}
+
+@property (strong, nonatomic) IBOutlet UISegmentedControl *segmentedControl;
 
 @end
 
 NSString * const feedCell = @"FeedCell";
+NSString * const articleCell = @"ArticleCell";
 NSString * const addAlertTitle = @"Add new feed";
 NSString * const addButtonTitle = @"Add";
 NSString * const cancelButtonTitle = @"Cancel";
@@ -25,7 +38,22 @@ NSString * const nameTextFieldPlaceholder = @"Enter the name of the feed";
 NSString * const addressTextFieldPlaceholder = @"Enter the address of the feed";
 NSString * const name = @"name";
 
+
+
 @implementation MasterViewController
+
+-(void)setUpExampleFeeds
+{
+    feeds = [NSMutableArray array];
+    Feed *feed = [Feed MR_createEntity];
+    feed.name = @"Apple news";
+    feed.feedURL = @"http://images.apple.com/main/rss/hotnews/hotnews.rss";
+    [feeds addObject:feed];
+    
+    feed.name = @"Example feed";
+    feed.feedURL = @"http://www.feedforall.com/sample.xml";
+    [feeds addObject:feed];
+}
 
 -(void)viewDidLoad
 {
@@ -34,39 +62,27 @@ NSString * const name = @"name";
     [self fetchFeeds];
 }
 
--(void)saveContext {
-    [[NSManagedObjectContext MR_defaultContext] MR_saveToPersistentStoreWithCompletion:^(BOOL success, NSError *error) {
-        if (success) {
-            NSLog(@"You successfully saved your context.");
-        } else if (error) {
-            NSLog(@"Error saving context: %@", error.description);
-        }
-    }];
-}
-
--(void)fetchFeeds
-{
-    self.feeds = [[Feed MR_findAllSortedBy:name ascending: YES] mutableCopy];
-}
-
 - (IBAction)changeSortKey:(id)sender {
     switch ([(UISegmentedControl*)sender selectedSegmentIndex]) {
         case 0: {
-            NSLog(@"Viewing feeds");
+            viewType = allFeeds;
+            
+            UIBarButtonItem *button = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemAdd target:self action:@selector (addFeed:)];
+            self.navigationItem.rightBarButtonItem = button;
+            
             [self.tableView reloadData];
         }
             break;
         case 1: {
-            NSLog(@"Viewing articles");
+            viewType = allArticles;
             [self.tableView reloadData];
+            [self.navigationItem setRightBarButtonItem:nil];
         }
             break;
         default:
             break;
     }
 }
-
-#pragma mark - AlertView
 
 -(IBAction)addFeed:(id)sender
 {
@@ -85,6 +101,33 @@ NSString * const name = @"name";
     [alertView show];
 }
 
+-(void)showAllArticles
+{
+    viewType = allArticles;
+    [self.navigationItem setRightBarButtonItem:nil];
+    
+    [self.tableView reloadData];
+}
+
+#pragma mark - CoreData
+
+-(void)saveContext {
+    [[NSManagedObjectContext MR_defaultContext] MR_saveToPersistentStoreWithCompletion:^(BOOL success, NSError *error) {
+        if (success) {
+            NSLog(@"You successfully saved your context.");
+        } else if (error) {
+            NSLog(@"Error saving context: %@", error.description);
+        }
+    }];
+}
+
+-(void)fetchFeeds
+{
+    feeds = [[Feed MR_findAllSortedBy:name ascending: YES] mutableCopy];
+}
+
+#pragma mark - AlertView
+
 -(void)alertView:(UIAlertView *)alertView clickedButtonAtIndex:(NSInteger)buttonIndex
 {
     if (alertView.tag == 0)
@@ -97,7 +140,7 @@ NSString * const name = @"name";
             textField = [alertView textFieldAtIndex:1];
             feed.feedURL = textField.text;
             
-            [self.feeds addObject:feed];
+            [feeds addObject:feed];
             [self saveContext];
             [self.tableView reloadData];
         }
@@ -108,31 +151,68 @@ NSString * const name = @"name";
 
 -(NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
 {
-    NSLog(@"Number of cells: %li", [self.feeds count]);
-    return [self.feeds count];
+    switch (viewType) {
+        case allFeeds:
+            return [feeds count];
+            break;
+            
+        default:
+            return [articles count];
+            break;
+    }
 }
 
 -(UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
 {
-    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:feedCell];
-    Feed *feed = [self.feeds objectAtIndex:indexPath.row];
-    cell.textLabel.text = feed.name;
-    
-    return cell;
+    switch (viewType) {
+        case allFeeds:
+        {
+            UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:feedCell];
+            Feed *feed = [feeds objectAtIndex:indexPath.row];
+            cell.textLabel.text = feed.name;
+            
+            return cell;
+        }
+            
+        default:
+        {
+            UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:articleCell];
+            
+            return cell;
+        }
+    }
 }
 
 -(BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath
 {
-    return YES;
+    if (viewType == allFeeds)
+    {
+        return YES;
+    }
+    return NO;
 }
 
 -(void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath
 {
     if (editingStyle == UITableViewCellEditingStyleDelete)
     {
-        [[NSManagedObjectContext MR_defaultContext] deleteObject:[self.feeds objectAtIndex:indexPath.row]];
-        [self.feeds removeObjectAtIndex:indexPath.row];
+        [[NSManagedObjectContext MR_defaultContext] deleteObject:[feeds objectAtIndex:indexPath.row]];
+        [feeds removeObjectAtIndex:indexPath.row];
         [self saveContext];
+        [self.tableView reloadData];
+    }
+}
+
+-(void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    if (viewType == allFeeds)
+    {
+        viewType = articlesOfAFeed;
+        self.segmentedControl.selectedSegmentIndex = 1;
+        [self.navigationItem setRightBarButtonItem:nil];
+        UIBarButtonItem *button = [[UIBarButtonItem alloc] initWithTitle:@"Show all" style:UIBarButtonItemStylePlain target:self action:@selector (showAllArticles)];
+        self.navigationItem.rightBarButtonItem = button;
+        
         [self.tableView reloadData];
     }
 }
